6271e6e72600e63334bee9429615ea91728778c1
Kris Hahn <kris.hahn@javs.com>
2017-12-01 11:45:07 -0500

    Initial Code

---
+++ TicketSale/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TicketSale.MvcApplication" Language="C#" %>
---
+++ TicketSale/Models/SchoolVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketSale.Models
+{
+    public class SchoolVM
+    {
+        public  int SchoolID { get; set; }
+        public string SchoolName { get; set; }
+    }
+}
