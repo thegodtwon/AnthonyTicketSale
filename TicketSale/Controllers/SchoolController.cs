5da8008b0d3531c3d93fb04f8f13544727079c4c
Anthony Richey <arichey39@gmail.com>
2017-12-02 11:39:41 -0500

    added School.cs class but with errors

--- TicketSale/Controllers/SchoolController.cs
+++ TicketSale/Controllers/SchoolController.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TicketSale.Models;
 
 namespace TicketSale.Controllers
 {
     public class SchoolController : Controller
     {
+        //private TicketSaleDb db = new TicketSaleDb();
+
         // GET: School
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+        // POST: School
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string schoolname)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    db.SchoolName.Add(schoolname);
+            //    db.SaveChanges();
+            //    return RedirectToAction("Index");
+            //}
             return View();
         }
     }
--- TicketSale/Models/SchoolVM.cs
+++ TicketSale/Models/SchoolVM.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace TicketSale.Models
 {
-    public class SchoolVM
+
+    namespace TicketSale.Models
     {
-        public  int SchoolID { get; set; }
-        public string SchoolName { get; set; }
+        public class SchoolVM
+        {
+            public int SchoolID { get; set; }
+            public string SchoolName { get; set; }
+        }
     }
+    
 }
