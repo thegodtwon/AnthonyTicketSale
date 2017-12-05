b95f01a459872619501422d1b92b6325629ba62d
Anthony Richey <arichey39@gmail.com>
2017-12-05 13:37:26 -0500

    added more stuff

--- TicketSale/Controllers/SellerController.cs
+++ TicketSale/Controllers/SellerController.cs
@@ -2,90 +2,31 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TicketSale.Models;
+using TicketSale.DataLayer;
 
 namespace TicketSale.Controllers
 {
     public class SellerController : Controller
     {
-        // GET: Seller
+
+        // GET: School
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-
-        // GET: Seller/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Seller/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Seller/Create
+        // POST: School
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(SellerModel sellername)
         {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            Seller.Create(sellername);
+            return View(sellername);
         }
-
-        // GET: Seller/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Seller/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Seller/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Seller/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
     }
 }
---
+++ TicketSale/DataLayer/Seller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using TicketSale.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace TicketSale.DataLayer
+{
+    public class Seller
+    {
+        public static void Create(SellerModel sellername)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["TicketSaleConn"].ConnectionString))
+                {
+                    connection.Open();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.Text;
+                        command.CommandText = string.Format("INSERT INTO Seller (LastName, FirstName) VALUES ('{0}', '{1}')", sellername.LastName, sellername.FirstName, DateTime.UtcNow);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
